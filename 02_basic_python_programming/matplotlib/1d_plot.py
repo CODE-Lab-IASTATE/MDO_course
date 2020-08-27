9b93b8ebd4f5eab4c73b384766c9085561b6f6f3
jethro <46826917+jethromoses@users.noreply.github.com>
2020-08-27 18:16:15 -0500

    Update 1d_plot.py

--- 02_basic_python_programming/matplotlib/1d_plot.py
+++ 02_basic_python_programming/matplotlib/1d_plot.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2, 100)
 
-# Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
-fig, ax = plt.subplots()  # Create a figure and an axes.
-ax.plot(x, x, label='linear')  # Plot some data on the axes.
-ax.plot(x, x**2, label='quadratic')  # Plot more data on the axes...
-ax.plot(x, x**3, label='cubic')  # ... and some more.
-ax.set_xlabel('x label')  # Add an x-label to the axes.
-ax.set_ylabel('y label')  # Add a y-label to the axes.
-ax.set_title("Simple Plot")  # Add a title to the axes.
-ax.legend()  # Add a legend.
+fig, ax = plt.subplots()  # Create figure and axes
+ax.plot(x, x, label='linear')  # Plot data
+ax.plot(x, x**2, label='quadratic')  
+ax.plot(x, x**3, label='cubic')  
+ax.set_xlabel('x label')  # x-label
+ax.set_ylabel('y label')  # y-label
+ax.set_title("Simple Plot")  # itle
+ax.legend()  # legend
